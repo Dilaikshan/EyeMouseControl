37365be1b8cdb45915f1a73883496927112c31f7
Dilaikshan <dilaikshan1445@gmail.com>
2025-04-11 22:08:51 +0530

    Detect landmarks

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import cv2 #import the respect opencv package
+import mediapipe as  mp # this is respect to detect face
+
 cam = cv2.VideoCapture(0) # declare cam used to capture video
+face_mesh = mp.solutions.face_mesh.FaceMesh()
 while True:
     _,frame = cam.read()
+    rgp_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgp_frame)
+    landmark_points = output.multi_face_landmarks
+    print(landmark_points)
     cv2.imshow('Eye control mouse', frame)
     cv2.waitKey(1)
