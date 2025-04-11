b53157bb7218767f4c13fc202909942d21187486
Dilaikshan <dilaikshan1445@gmail.com>
2025-04-11 22:31:14 +0530

    Update main.py
    Detect one eye

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2 #import the respect opencv package
 import mediapipe as  mp # this is respect to detect face
 
 cam = cv2.VideoCapture(0) # declare cam used to capture video
-face_mesh = mp.solutions.face_mesh.FaceMesh()
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 while True:
     _,frame = cam.read()
     rgp_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -13,7 +13,7 @@ while True:
 
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        for landmark in landmarks:
+        for landmark in landmarks[474:478]:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame,(x,y),3,(0,255,0))
